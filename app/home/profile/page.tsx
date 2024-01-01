2089c3c4dba32a41c03e1f7c152a8778ca8ecee4
Boris <72118807+Curis-lab@users.noreply.github.com>
2024-01-01 10:00:57 +0630

    feat: upgrade profile page

--- app/home/profile/page.tsx
+++ app/home/profile/page.tsx
@@ -1,16 +1,26 @@
-
-
+import { getCurrentUser } from "@/app/actions/getCurrentUser";
 import Modal from "@/components/modal/modals";
 import ProfileModal from "@/components/modal/profile-modal";
-
+import { Department } from "@prisma/client";
 
 const Profile = async () => {
+  const currentUser = await getCurrentUser();
+
   
-  return (
-    <Modal isOpen={true} className="w-2/3 p-10">
-        <ProfileModal/>
-    </Modal>
-  );
+  if (currentUser) {
+    return (
+      <Modal isOpen={true} className="w-2/3 p-10">
+        <ProfileModal currentUser={currentUser?.profile} />
+      </Modal>
+    );
+  }
+  else{
+    return(
+      <Modal isOpen className="w-2/3 p-10">
+        <h1>Noting More </h1>
+      </Modal>
+    )
+  }
 };
 
 export default Profile;
