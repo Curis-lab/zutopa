c6536b27ce713fc6052f8f8cb814e53461541ee9
Boris <72118807+Curis-lab@users.noreply.github.com>
2024-01-05 01:19:07 +0630

    feat: new search bar

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -3,6 +3,7 @@ import { getAllUser, getOtherUsers } from "../actions/getOtherUser";
 import { getZutos } from "../actions/getZutos";
 import { getCompatableResource } from "../actions/getCompatibleResource";
 import Zudo from "@/components/zudo";
+import SearchBar from "@/components/search-bar";
 
 const Home = async () => {
   const otherUsers = await getOtherUsers();
@@ -15,6 +16,7 @@ const Home = async () => {
     <div className="h-full flex">
       <UserPanel users={otherUsers} />
       <div className="flex-1 flex flex-col">
+        <SearchBar/>
         <div className="w-full p-10 flex flex-col gap-y-4">
           {g && g.map((g) => <Zudo profile={g.profile} zuto={g.zuto} />)}
         </div>
