53b0b1a693f58303e437cd343fd5b280f2309317
Boris <72118807+Curis-lab@users.noreply.github.com>
2024-02-08 22:08:37 +0630

    to continue on route file

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -11,15 +11,16 @@ const S3client = new S3Client({
 
 export const dynamic = "force-dynamic";
 
-async function uploadFilesToS3(file:any, fileName:any){
+async function uploadFilesToS3(file: Buffer, fileName: string) {
   const fileBuffer = file;
-  console.log(fileName);
+
+  //!Error is happening here
   const params = {
     Bucket: process.env.ZUDO_BUCKET_NAME,
-    Key:`${fileName}`,
-    Body:fileBuffer,
-    ContentType:"image/jpg"
-  }
+    Key: `${fileName}`,
+    Body: fileBuffer,
+    ContentType: "image/jpg",
+  };
   const command = new PutObjectCommand(params);
   await S3client.send(command);
   return fileName;
@@ -27,21 +28,15 @@ async function uploadFilesToS3(file:any, fileName:any){
 
 export async function POST(request: NextRequest) {
   try {
-    console.log('posting in first step');
     const formData = await request.formData();
     const file = formData.get("profile-pic") as File;
-
-    console.log('posting in second step');
-
-    if(!file){
-      return NextResponse.json({error: "file is required"},{status:400});
+    if (!file) {
+      return NextResponse.json({ error: "file is required" }, { status: 400 });
     }
-    
-    const buffer = Buffer.from( await file.arrayBuffer());
+    const buffer = Buffer.from(await file.arrayBuffer());
     const fileName = await uploadFilesToS3(buffer, file.name);
-    console.log('final steps');
-    return NextResponse.json({ success: true, fileName },{status:200});
+    return NextResponse.json({ success: true, fileName }, { status: 200 });
   } catch (error) {
-    return NextResponse.json({ error },{status: 404});
+    return NextResponse.json({ error }, { status: 404 });
   }
 }
